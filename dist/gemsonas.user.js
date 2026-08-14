048c454f10ec41627fee65e4be23f51c59c9b1e9
JadeMin <36400787+JadeMin@users.noreply.github.com>
2026-08-15 01:28:00 +0900

    upload: upload test

--- dist/gemsonas.user.js
+++ dist/gemsonas.user.js
@@ -3,6 +3,7 @@
 // @description	Gemsonas
 // @version		0.0.0
 // @match		https://gemini.google.com/*
+// @match		https://chatgpt.com/*
 // @updateURL	https://github.com/KlartNET-UserScripts/gemsonas/raw/refs/heads/main/dist/gemsonas.user.js
 // @downloadURL	https://github.com/KlartNET-UserScripts/gemsonas/raw/refs/heads/main/dist/gemsonas.user.js
 // @connect		raw.githubusercontent.com
