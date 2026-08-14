e37e34b8f27bea67aa54e4a5016ad71bec3a7842
JadeMin <36400787+JadeMin@users.noreply.github.com>
2026-08-15 01:28:26 +0900

    feat: add ChatGPT support

--- build.js
+++ build.js
@@ -3,7 +3,7 @@ import esbuild from "esbuild";
 const banner = `// ==UserScript==
 // @name		Gemsonas
 // @description	Gemsonas
-// @version		0.0.0
+// @version		0.0.1
 // @match		https://gemini.google.com/*
 // @match		https://chatgpt.com/*
 // @updateURL	https://github.com/KlartNET-UserScripts/gemsonas/raw/refs/heads/main/dist/gemsonas.user.js
--- dist/gemsonas.user.js
+++ dist/gemsonas.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		Gemsonas
 // @description	Gemsonas
-// @version		0.0.0
+// @version		0.0.1
 // @match		https://gemini.google.com/*
 // @match		https://chatgpt.com/*
 // @updateURL	https://github.com/KlartNET-UserScripts/gemsonas/raw/refs/heads/main/dist/gemsonas.user.js
